347804e24fcf3d4680b3860030ed269bc6e73bed
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-05-18 01:23:25 +0900

    fix: sprockets cache

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,11 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.configure do |env|
+    env.cache = Sprockets::Cache::FileStore.new(
+      ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
+      Rails.application.config.assets.cache_limit,
+      env.logger
+    )
+end
