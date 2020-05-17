2d94dd74615f3d8b428909f98c7599fb2a273302
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-05-17 22:30:47 +0900

    add: Top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'root/index'
+
+  root to: 'root#index'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
