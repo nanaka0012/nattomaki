ce2a023ae56e6f0d3e8db3dbd07c17a35090e979
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-05-24 01:15:36 +0900

    add: ユーザー認証 view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+
+
+    protected
+
+    # 入力フォームからアカウント名情報をDBに保存するために追加
+    def configure_permitted_parameters
+      added_attrs = [ :name, :email, :password, :password_confirmation　]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    end 
 end
