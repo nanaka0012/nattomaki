0dc20d2940c178848e8531f99cea561bd53b81fa
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-05-25 21:01:10 +0900

    fix: route error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
 
     def after_sign_in_path_for(resource)
-        "/user/#{current_user.id}"
+        "/users/#{current_user.id}"
     end
 
     protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
-
-  get 'novels/index'
-  get 'novels/show'
-  get 'novels/new'
+  
+  
   root 'root#index'
+  get 'mypage', :to => "mypage#index"
+  
   resources :pennames
   resources :stories, only: [:index, :show, :new]
   resources :novels, only: [:index, :show, :new]
   
-  get 'mypage', :to => "mypage#index"
-  get "users/:id", :to => "mypage#index"
-
   devise_scope :user do
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
-
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
   }
-
+  
+  get "users/:id", :to => "mypage#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
