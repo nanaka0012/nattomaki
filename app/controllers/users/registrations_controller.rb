3322cba1638bc3761f25b1cb5904318477022c39
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-06-01 16:00:32 +0900

    fix: rooting error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    mypage_top_path
+    mypage_path
   end
 
   # The path used after sign up for inactive accounts.
