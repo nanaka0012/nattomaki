adda5b64d0488313a98551b8cf8d57f8b41f80a5
yuta <uta.betty626@gmail.com>
2020-06-20 00:27:03 +0900

    update: seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,12 @@
 
 user = User.create(name: 'test', email: 'test@test.com', password: 'abcd1234')
 
-novel = Novel.create(title: 'TEST_1', genre: 'sf')
-novel = Novel.create(title: 'TEST_2', genre: 'sf')
+novel = Novel.create(title: 'TEST_1', genre: 2, series: 7)
+novel = Novel.create(title: 'TEST_2', genre: 3, series: 7)
 
-story = Story.create(subtitle: 'test1', novel_id: 1, content: 'てすと')
-story = Story.create(subtitle: 'test2', novel_id: 1, content: 'てすとだよ')
-story = Story.create(subtitle: 'test3', novel_id: 2, content: 'てすとやで～～～')
-story = Story.create(subtitle: 'test4', novel_id: 2, content: 'てすとです～')
+story = Story.create(penname: 'ペンネーム', subtitle: 'TEST_1の一話', novel_id: 1, content: 'てすと')
+story = Story.create(penname: 'ペンネーム', subtitle: 'TEST_1の二話', parent_id: 1, novel_id: 1, content: 'てすとだよ')
+story = Story.create(penname: 'ペンネーム2', subtitle: 'TEST_1の改二話', parent_id: 1, novel_id: 1, content: 'てすとー')
+story = Story.create(penname: 'ペンネーム2', subtitle: 'TEST_1の改三話', parent_id: 3, novel_id: 1, content: 'てすとや')
+story = Story.create(penname: 'ペンネーム3', subtitle: 'TEST_2の一話', novel_id: 2, content: 'てすとやで～～～')
+story = Story.create(penname: 'ペンネーム3', subtitle: 'TEST_2の二話', parent_id: 5, novel_id: 2, content: 'てすとです～')
