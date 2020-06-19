49a86c0cac52b75c31510b17c745cd7e66c47ceb
yuta <uta.betty626@gmail.com>
2020-06-20 00:32:08 +0900

    update:root_controller Novelクラスのscopeを持ってくるためのアクション

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,12 @@
 class RootController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
+    @novels = Novel.all
+    if params[:title].present?
+      @novels = @novels.get_by_title params[:title]
+    end
+    if params[:genre].present?
+      @novels = @novels.get_by_genre params[:genre]
+    end
   end
 end
