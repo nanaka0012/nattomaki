e5501035aa460981e45deb136b567115f8f8b828
yuta <uta.betty626@gmail.com>
2020-06-20 00:30:35 +0900

    update:model/novel.rb titleとgenreを絞り込むためのscope定義

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -5,4 +5,13 @@ class Novel < ApplicationRecord
     validates :genre, presence: true
     validates :title, presence: true
     validates :series, presence: true, :numericality => { :less_than_or_equal_to => 10, :greater_than => 1 }
+    
+    scope :get_by_title, ->(title) {
+    where("title like ?", "%#{title}%")
+    }
+    
+    scope :get_by_genre, ->(genre) {
+    where(genre: genre)
+    }
+    
 end
